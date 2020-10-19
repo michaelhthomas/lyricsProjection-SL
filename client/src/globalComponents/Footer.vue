1c7dd796b8326b7b4d43d7cb80b2a86ac6275fe5
Michael Thomas <michaelhthomas@outlook.com>
2020-10-19 18:15:17 -0400

    Translate footer

--- client/src/globalComponents/Footer.vue
+++ client/src/globalComponents/Footer.vue
@@ -3,11 +3,19 @@
       <v-btn :to="{ name: 'About'}"  icon>
         <v-icon>mdi-information</v-icon>
       </v-btn>
-      <div>Župnija Šempeter pri Gorici</div>
+      <div>{{ $t('footerText') }}</div>
       <v-spacer></v-spacer>
       <div> {{ new Date().getFullYear() }}</div>
     </v-footer>
 </template>
+
+<i18n>
+en:
+  footerText: "Parish of Šempeter pri Gorici"
+sl:
+  footerText: "Župnija Šempeter pri Gorici"
+</i18n>
+
 <script>
 export default {
   name: 'Footer'
